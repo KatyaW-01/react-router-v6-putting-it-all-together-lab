61d588e23e7a41a798df220eb14c08465bb524b4
Katya Weicht <katya.weicht@gmail.com>
2025-06-13 12:08:29 -0600

    Create routes in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import DirectorContainer from "./pages/DirectorContainer"
+import DirectorCard from "./pages/DirectorCard"
+import MovieCard from "./pages/MovieCard"
+
 const App = () => {
     return (
         <>
-        {/* define all routes here */}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/directors" element={<DirectorContainer />} >
+                    <Route path=":id" element={<DirectorCard />} />
+                    <Route path="movies/:movieId" element={<MovieCard />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
         </>
     )
 }
